010e920e40be7f4aee0f0b5062f1c207f3f68171
Nicolas Aguilera Garcia <niagar25@gmail.com>
2023-08-12 16:52:21 -0500

    [ADD] DB Model

--- BooksLibrary.Api/Program.cs
+++ BooksLibrary.Api/Program.cs
@@ -1,3 +1,5 @@
+using BooksLibrary.Model;
+
 namespace BooksLibrary.Api
 {
     public class Program
@@ -7,9 +9,9 @@ namespace BooksLibrary.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSqlServer<BooksLibraryContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
---
+++ BooksLibrary.Model/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLibrary.Model.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int Pages { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        public string? CoverUrl { get; set; }
+        public string? Isbn { get; set; }
+        public string? Year { get; set; }
+        public Genre[] Genres { get; set; }
+        public int AuthorId { get; set; }
+
+
+        public virtual Author Author { get; set; }
+        public virtual ICollection<RelationUserBook> RelationUsers { get; set; }
+    }
+}
